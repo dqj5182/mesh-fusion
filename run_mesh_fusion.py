2efa2ee87ea4d783fa61fd50f53e34725273e02a
dqj5182 <dqj5182@snu.ac.kr>
2024-05-25 07:29:41 +0000

    update

--- run_mesh_fusion.py
+++ run_mesh_fusion.py
@@ -1,8 +1,12 @@
 import os
 
-in_mesh_dir = 'data/MOW/data/models/board_food_v_LUS1jeTGc68_frame000082.obj'
+in_off_mesh_dir = 'examples/0_in'
+scaled_mesh_dir = 'examples/1_scaled'
+depth_mesh_dir = 'examples/2_depth'
+watertight_mesh_dir = 'examples/2_watertight'
+output_dir = 'examples/3_out'
 
-os.system('python 1_scale.py --in_dir=examples/0_in/ --out_dir=examples/1_scaled/')
-os.system('xvfb-run -a python 2_fusion.py --mode=render --in_dir=examples/1_scaled/ --depth_dir=examples/2_depth/ --out_dir=examples/2_watertight/')
-os.system('python 2_fusion.py --mode=fuse --in_dir=examples/1_scaled/ --depth_dir=examples/2_depth/ --out_dir=examples/2_watertight/')
-os.system('xvfb-run -a python 3_simplify.py --in_dir=examples/2_watertight/ --out_dir=examples/3_out/')
+os.system(f'python 1_scale.py --in_dir={in_off_mesh_dir} --out_dir={scaled_mesh_dir}')
+os.system(f'xvfb-run -a python 2_fusion.py --mode=render --in_dir={scaled_mesh_dir} --depth_dir={depth_mesh_dir} --out_dir={watertight_mesh_dir}')
+os.system(f'python 2_fusion.py --mode=fuse --in_dir={scaled_mesh_dir} --depth_dir={depth_mesh_dir} --out_dir={watertight_mesh_dir}')
+os.system(f'xvfb-run -a python 3_simplify.py --in_dir={watertight_mesh_dir} --out_dir={output_dir}')
